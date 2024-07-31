ab6520f156ee82ce605e13a9be395ea18f6b5ebf
Alves <josealvesfn@gmail.com>
2024-07-31 01:13:53 -0300

    atualizando projeto

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.principal.Principal;
-import br.com.alura.screenmatch.service.ConsumoAPI;
-import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,86 +18,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
-		// Instanciando objeto da classe de consumo de API
-		ConsumoAPI consumoAPI = new ConsumoAPI();
-
-		// Instanciando objeto da classe de conversão do json
-		// Detalhe: o método obterDados é genérico, o que é ótimo para receber objetos distintos para desserializar
-		ConverteDados conversor = new ConverteDados();
-
-		// Consumindo e executando o conversor de dados de serie
-		String json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=45aad444");
-		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
-
-		// Consumindo e executando o conversor de dados de episodios
-		json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=45aad444");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		// Consumindo e executando o conversor de dados de temporadas
-
-		// Criando uma lista para receber as informações de cada temporada
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		// Populando o objeto dadosTemporada e usando dadosSerie.totalTemporadas() para limitar o for
-		for(int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-			json = consumoAPI.obterDados("http://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=45aad444");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-
-		}
-
-		// Imprimindo as informações dos episódios por temporada
-		for (DadosTemporada temporada: temporadas) {
-			System.out.println(temporada);
-		}
-//		temporadas.forEach(System.out::println);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//		String cafe = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(cafe);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,23 +1,52 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
-    // Instanciando um objeto para leitura + objeto para ConsumoAPI e deixando como privado dentro de um atributo de classe
+    // Instanciando objetos que serão chamados pela classe
     private Scanner leitura = new Scanner(System.in);
     private ConsumoAPI consumo = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
 
     // Definindo atributos constantes relativos aos endereço de busca e chave da API
     private final String ENDERECO = "http://www.omdbapi.com/?t=";
-    private final String API_KEY = "&45aad888";
+    private final String API_KEY = "&apikey=45aad444";
 
-    // Definindo o método usado para buscar as informações da série
-    public void exibeMenu(){
+    // Definindo o método usado para buscar as informações da série, temporadas, etc
+    public void exibeMenu() {
         System.out.println("Digite o nome da série que deseja: ");
         String nomeSerie = leitura.nextLine();
         String json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dadosSerie);
+
+        // Criando uma lista para receber as informações de cada temporada
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        // Populando o objeto dadosTemporada e usando dadosSerie.totalTemporadas() para limitar o for
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+
+        // Imprimindo as informações dos episódios por temporada
+        for (DadosTemporada temporada : temporadas) {
+            System.out.println(temporada);
+        }
+
+// 		temporadas.forEach(System.out::println);
+//      http://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=45aad444
+
+
+
     }
 }
