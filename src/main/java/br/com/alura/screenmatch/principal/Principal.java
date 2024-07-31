fec2f3c49cf44b393989c03e9ea4cc85a6c0c729
jose-alves-fn <josealvesfn@gmail.com>
2024-07-31 17:30:16 -0300

    atualização do projeto

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -6,6 +6,7 @@ import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,30 +23,43 @@ public class Principal {
 
     // Definindo o método usado para buscar as informações da série, temporadas, etc
     public void exibeMenu() {
-        System.out.println("Digite o nome da série que deseja: ");
-        String nomeSerie = leitura.nextLine();
-        String json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
-        System.out.println(dadosSerie);
+//        System.out.println("Digite o nome da série que deseja: ");
+//        String nomeSerie = leitura.nextLine();
+//        String json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+//        DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+//        System.out.println(dadosSerie);
+//
+//        // Criando uma lista para receber as informações de cada temporada
+//        List<DadosTemporada> temporadas = new ArrayList<>();
+//
+//        // Populando o objeto dadosTemporada e usando dadosSerie.totalTemporadas() para limitar o for
+//        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
+//            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+//            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//            temporadas.add(dadosTemporada);
+//        }
+//
+//        // Imprimindo as informações dos episódios por temporada
+//        // Para cada temporada, imrpima o conteúdo
+//        temporadas.forEach(System.out::println);
+//
+//        // Usando lambdas para imprimir todos os episodios por nome
+//        // Para cada temporada t em temporadas, retorne a lista de episodios.
+//        // Para cada episodio e na lista de episodios, imprima o titulo.
+//        temporadas.forEach(t -> t.episodio().forEach(e -> System.out.println(e.titulo())));
 
-        // Criando uma lista para receber as informações de cada temporada
-        List<DadosTemporada> temporadas = new ArrayList<>();
 
-        // Populando o objeto dadosTemporada e usando dadosSerie.totalTemporadas() para limitar o for
-        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
-            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-            temporadas.add(dadosTemporada);
-        }
+        List<String> nomes = Arrays.asList("liliam", "marco", "josé", "ana paula", "geanny", "nívea");
 
-        // Imprimindo as informações dos episódios por temporada
-        // Para cada temporada, imrpima o conteúdo
-        temporadas.forEach(System.out::println);
+        // Stream
+        nomes.stream()
+                .sorted() // Op. intermediária
+                .limit(3) // Op. intermediária
+                .filter(n -> n.startsWith("a")) // Op. intermediária
+                .map(n -> n.toUpperCase())
+                .forEach(System.out::println);// Op. final
 
 
-        // Usando lambdas para imprimir todos os episodios por nome
-        // Para cada temporada t em temporadas, retorne a lista de episodios.
-        // Para cada episodio e na lista de episodios, imprima o titulo.
-        temporadas.forEach(t -> t.episodio().forEach(e -> System.out.println(e.titulo())));
+
     }
 }
